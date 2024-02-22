934d3346bd7555f006bc561d3624063e05e3ddf8
Saksham Singla <sakshamsingla21@gmail.com>
2024-02-22 13:52:15 +0530

    initial commit

--- app.py
+++ app.py
@@ -41,8 +41,21 @@ app.secret_key='abcxyz'
 
 import firebase_admin
 from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("jobscrapiee-firebase-adminsdk-1n722-bec08b779e.json")
+cred=credentials.Certificate({
+  "type": os.getenv("TYPE"),
+  "project_id":os.getenv("PROJECT_ID"),
+  "private_key_id":os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY"),
+  "client_email":os.getenv("CLIENT_EMAIL") ,
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri": os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url":os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+  "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+  "universe_domain":os.getenv("UNIVERSE_DOMAIN")
+}
+)
+#cred = credentials.Certificate("jobscrapiee-firebase-adminsdk-1n722-bec08b779e.json")
 #cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -170,3 +183,5 @@ def contact():
     return render_template('contact.html')       
 
 
+# if __name__=='__main__':
+#     app.run(debug=True)
