83ac340486d383bc290f01ba9360036d080e0bf2
Saksham Singla <sakshamsingla21@gmail.com>
2024-02-22 14:05:32 +0530

    initial commit

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ cred=credentials.Certificate({
   "auth_provider_x509_cert_url":os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
   "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
   "universe_domain":os.getenv("UNIVERSE_DOMAIN")
-}
-)
+})
 
 firebase_admin.initialize_app(cred)
 db = firestore.client()
