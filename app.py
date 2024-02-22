af6460ebfa8f991f90afe3ba3f1886cdf3c660ff
Saksham Singla <sakshamsingla21@gmail.com>
2024-02-22 11:12:21 +0530

    initial commit

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ app.secret_key='abcxyz'
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# cred = credentials.Certificate("jobscrapiee-firebase-adminsdk-1n722-bec08b779e.json")
-cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+cred = credentials.Certificate("jobscrapiee-firebase-adminsdk-1n722-bec08b779e.json")
+#cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 @app.route('/')
