d8c8158d3aaa243d4116863825a8ec83888f4355
Saksham Singla <sakshamsingla21@gmail.com>
2024-02-23 17:09:46 +0530

    .

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ cred = credentials.Certificate({
     "type": "service_account",
     "project_id": os.getenv("PROJECT_ID"),
     "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-    "private_key": os.getenv("PRIVATE_KEY"),
+    "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
     "client_email": os.getenv("CLIENT_EMAIL"),
     "client_id": os.getenv("CLIENT_ID"),
     "auth_uri": os.getenv("AUTH_URI"),
