c509f3474688de85b96d9218659126fb905e94d2
Saksham Singla <sakshamsingla21@gmail.com>
2024-02-23 16:43:57 +0530

    initial commit

--- app.py
+++ app.py
@@ -12,19 +12,26 @@ load_dotenv()
 app.secret_key='abcxyz'
 
 
-cred=credentials.Certificate({
-  "type": os.getenv("TYPE"),
-  "project_id":os.getenv("PROJECT_ID"),
-  "private_key_id":os.getenv("PRIVATE_KEY_ID"),
-  "private_key": os.getenv("PRIVATE_KEY").replace(r'\n', '\n'),
-  "client_email":os.getenv("CLIENT_EMAIL") ,
-  "client_id": os.getenv("CLIENT_ID"),
-  "auth_uri": os.getenv("AUTH_URI"),
-  "token_uri": os.getenv("TOKEN_URI"),
-  "auth_provider_x509_cert_url":os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-  "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
-  "universe_domain":os.getenv("UNIVERSE_DOMAIN")
+
+private_key = os.getenv("PRIVATE_KEY")
+
+if private_key is not None:
+    private_key = private_key.replace(r'\n', '\n')
+
+cred = credentials.Certificate({
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": private_key,
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN")
 })
+
 
 
 # cred=credentials.Certificate("firebase-key.json")
